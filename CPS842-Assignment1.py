f90ae3e7eb84be2a1ea1529215184fed073ea262
Majuth <majuth101@hotmail.com>
2022-10-04 17:42:01 -0400

    changed to pandas

--- CPS842-Assignment1.py
+++ CPS842-Assignment1.py
@@ -1,18 +1,14 @@
-import json
+from turtle import title
+import pandas as pd 
+from bs4 import BeautifulSoup
 
-filename = "20Lines2.json"
-with open(filename, 'r', encoding='utf-16') as documentsJson:
-    lines = documentsJson.readlines()
-
+corpus_df = pd.read_json("20Lines2.json", encoding="utf-16", lines=True)
+print(corpus_df)
 dictionary = {}
 postings = {}
-
-for lineNumber in range(len(lines)):
-    currentLine = lines[lineNumber]
-    documentJsonObject = json.loads(currentLine)
-
-    titleAndText = documentJsonObject['title'] + ' ' + documentJsonObject['contents']
-    terms = titleAndText.lower().split()
+for i in range (0, len(corpus_df)-1):
+    titleAndContent = corpus_df.title[i] .join(' ').join(BeautifulSoup(corpus_df.contents[i], "html.parser").stripped_strings)
+    terms = titleAndContent.lower().split()
 
     for term in terms:
         if term in dictionary:
