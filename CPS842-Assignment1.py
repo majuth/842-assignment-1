a153fceacd112ceab7a73c7dad5678ed64ddc905
sakship2000 <sakshipadhiar2000@gmail.com>
2022-10-13 14:00:33 -0400

    cleaned up terms

--- CPS842-Assignment1.py
+++ CPS842-Assignment1.py
@@ -9,10 +9,23 @@ for i in range (0, len(corpus_df)-1):
     titleAndContent = corpus_df.title[i] .join(' ').join(BeautifulSoup(corpus_df.contents[i], "html.parser").stripped_strings)
     terms = titleAndContent.lower().split()
 
+# remove punctuation and symbols
+    filtered_terms = []
+    acceptable_characters = set('-0123456789abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ')
     for term in terms:
+        answer = ''.join(filter(acceptable_characters.__contains__, term))
+        filtered_terms.append(answer)
+
+    # sort alphabetically
+    filtered_terms=sorted(filtered_terms)
+
+# create dictionary
+    for term in filtered_terms:
         if term in dictionary:
             dictionary.update({term: dictionary[term]+1})
         else:
             dictionary[term] = 1
+
+# TODO: remove words that aren't words LOL
 
 print(dictionary)
